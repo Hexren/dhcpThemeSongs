c5d2c111f63eb62b64fdb1fce6b2df9954f8f143
Oliver Steenbuck <git@hexren.net>
2014-03-18 22:53:56 +0100

    add function for enriching data with know hw adresses

--- dhcpevent.py
+++ dhcpevent.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
-#from soco import SoCo
+
 from sys import argv
 import os
 import MySQLdb
+import syslog
+
+#add hardware address from last commit
+def addHw(data, cur):
+    try:
+        cur.execute("SELECT hw FROM events WHERE action = 'commit' AND ip = %(ip)s ORDER BY tm ASC LIMIT 1", data)
+        if cur.rowcount > 0:
+            row = cur.fetchone()
+            return row[0]
+        else:
+            return ''
+    except MySQLdb.Error, e:
+        try:
+			syslog.syslog(syslog.LOG_ERR, "MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+        except IndexError:
+            syslog.syslog(syslog.LOG_ERR, "MySQL Error: %s" % str(e))
+    exit(233)
+			
+	
+
+
 
 if __name__ == '__main__':
+        syslog.syslog('Processing started')
         newpid = os.fork()
-        
         if newpid == 0:
-                db = MySQLdb.connect(host="192.168.1.83", # your host, usually localhost
-                                             user="dhcp", # your username
-                                              passwd="sdsiuh347sdf435", # your password
-                                              db="dhcp_log") # name of the data base
+				#from soco import SoCo
+                db = MySQLdb.connect(host="192.168.1.83", user="dhcp", passwd="sdsiuh347sdf435", db="dhcp_log")
 
                 # you must create a Cursor object. It will let
                 #  you execute all the queries you need
@@ -23,5 +42,7 @@ if __name__ == '__main__':
                 if len(argv) >= 4:
                         data['hw'] = argv[3] 
                 else:
-                        data['hw'] = '' 
+                        data['hw'] = addHw(data, cur) 
                 cur.execute("INSERT INTO events (action, ip, hw) VALUES (%(action)s, %(ip)s, %(hw)s)", data)  
+
+	
